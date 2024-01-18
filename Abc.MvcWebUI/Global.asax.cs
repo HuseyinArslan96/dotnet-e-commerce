759333f63f79b5cb4fea77cf91831a7b932dc22d
HuseyinArslan96 <06.huseyinarslan@gmail.com>
2024-01-18 15:15:41 +0300

    Proje dosyası ekle.

---
+++ Abc.MvcWebUI/Global.asax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Abc.MvcWebUI.Entity;
+using Abc.MvcWebUI.Identity;
+
+namespace Abc.MvcWebUI
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            Database.SetInitializer(new DataInitializer());
+            Database.SetInitializer(new IdentityInitializer());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# dotnet-e-commerce
+C# programlama dili ve ASP.NET platformunda MVC tasarım deseni ve ORM tekniğiyle 
+Entity Framework kullanılarak inşa edilen bir e-ticaret sitesi prototipi: Bu sitede kullanıcılar üyelik oluşturabiliyor, üyelik 
+oluşturduktan sonra ürünleri inceleyip sipariş verebiliyor,
+diledikleri ürünü sepete ekleme veya sepetten çıkarma işlemini 
+yapabiliyor, toplam sipariş tutarını görüntüleyebiliyor, verdikleri 
+siparişi takip edebiliyor.
+![](e-ticaret.png)
+![](e-ticaret2.png)
+![](e-ticaret3.png)
+![](e-ticaret4.png)
